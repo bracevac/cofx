b9bd4763e019076ec05ae158d6c2a709141ca98c
Vaastav Arora <arora74@purdue.edu>
2021-05-26 16:35:53 -0400

    concrete Nat CoeffScalar implementation added

--- src/main/scala/interp/DecompTest.scala
+++ src/main/scala/interp/DecompTest.scala
@@ -2,6 +2,7 @@ package interp
 
 import scala.compiletime.{summonFrom, error, erasedValue, summonInline}
 import scala.util.NotGiven
+import scala.compiletime.ops.int._
 /**
  * A study on how to express coeffect scalar splitting at the type and term level.
  */
@@ -39,18 +40,34 @@ object DecompNew :
     type one = 1
 
     case class NAddZ[r <:Int]() extends Add[zero, r, r]
-    //case class NAddStep[l <: Int, r <:Int, res <:Int](prev : Add[l,r,res]) extends Add[S[l], r, S[res]]
-    //type S not found
+    case class NAddStep[l <: Int, r <:Int, res <:Int](prev : Add[l,r,res]) extends Add[S[l], r, S[res]]
+    trait NAddLvl1 :
+      given addz[r<:Int] : Add[zero,r,r] = NAddZ()
+
+    object NAdd extends NAddLvl1 :
+      given addstep[l <: Int, r <:Int, res <:Int](using prev : Add[l,r,res]) : Add[S[l],r,S[res]] = NAddStep(prev)
+    
+    case class NMulZLeft[r <:Int]() extends Mul[zero, r, zero]
+    case class NMulZRight[l <: Int]() extends Mul[S[l], zero, zero]
+    case class NMulStep[l <: Int, r <:Int, res <:Int, sum <:Int](add : Add[S[r],res,sum], prev : Mul[l,S[r],res]) extends Mul[S[l],S[r],sum]
+    
+    trait NMulLvl2 :
+      given mulzl[r <: Int] : Mul[zero, r, zero] = NMulZLeft()
+    trait NMulLvl1 extends NMulLvl2 :
+      given mulzr[l <: Int] : Mul[S[l],zero,zero] = NMulZRight()
+    object NMul extends NMulLvl1 :
+      given mulstep[l <: Int, r <:Int, res <:Int, sum <:Int](using prev : Mul[l,S[r],res], add : Add[S[r],res,sum]) : Mul[S[l],S[r],sum] = NMulStep(add,prev)
 
   trait Ctx
 
-  trait CoeffShape[C, S, CoeffScalar[C]]:
+  /* S: 
+  trait CoeffShape[S, C]:
 
     type zero <: S
     type one <: S
 
-    def dup(s1: S, s2: S): CoeffShape[C, S, CoeffScalar]
-    def extract(s: S): CoeffScalar[C]
+    def dup[C](using CoeffScalar[C].Add): CoeffShape[S]
+    //def extract(s: S): CoeffScalar[C]*/
 
 
 end DecompNew
