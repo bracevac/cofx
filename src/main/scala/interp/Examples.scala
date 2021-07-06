2b2986e539f9e7f1d8cf65f71b683de194699c25
Vaastav Arora <arora74@purdue.edu>
2021-07-05 20:27:56 -0700

    dup issue

--- src/main/scala/interp/Examples.scala
+++ src/main/scala/interp/Examples.scala
@@ -8,7 +8,7 @@ import scala.compiletime.{summonFrom, error, erasedValue, summonInline}
 
 object Scalars: 
 
-    implicit object NatScalar extends CoeffScalar[Int]:
+    implicit object natScalar extends CoeffScalar[Int]:
         type zero = 0
         type one = 1
 
@@ -35,33 +35,44 @@ object Scalars:
 object Vectors extends App:
 
     import Scalars._
+    import natScalar._
 
-    trait VecIC[n <: Int, T](using val s: CoeffScalar[Int]):
-        def take[i<:Int,j<:Int](using this.s.Add[i, j, n]): VecIC[i,T]
-        def drop[i<:Int,j<:Int](using this.s.Add[i, j, n]): VecIC[j,T]
-        //def dup[i<:Nat,j<:Nat](using mul : NMul[i,j,n]): VecIC[i, VecIC[j,T]] = mul match
-        //    case NMulZLeft(j)        => VNil
-        //    case NMulStep(prev,add)  => VCons(take(using add),drop(using add).dup(using prev))
+    trait VecIC[n <: Int, +T]:
+        
+        def take[i<:Int,j<:Int](using Add[i, j, n]): VecIC[i,T]
+        def drop[i<:Int,j<:Int](using Add[i, j, n]): VecIC[j,T]
+        def dup[i<:Int,j<:Int](using mul : Mul[i,j,n]): VecIC[i, VecIC[j,T]] = mul match
+            case _:NMulZLeft[_]        => VNil
+            case NMulStep(add, prev)  => VCons(take(using add),drop(using add).dup(using prev))
         //def extract(using NotGiven[n =:= Z.type]): T
-    
-    case object VNil extends VecIC[s.zero, Nothing]:
-        def take[i<:Int,j<:Int](using add : this.s.Add[i, j, s.zero]): VecIC[i,Nothing] = ???//add match
-            //case NAddZ(_) => VNil     
-        def drop[i<:Int,j<:Int](using add : this.s.Add[i, j, s.zero]): VecIC[j,Nothing] = ???//add match
-            //case NAddZ(_) => VNil
-    /*
-    case class VCons[n<:Nat,+T](hd : T, tl: Vec[n,T]) extends Vec[Suc[n], T] :
-        def take[i<:Int,j<:Int](using add : this.s.Add[i,j,Suc[n]]): VecIC[i,T] = add match 
-            case NAddZ(_) => VNil
+
+
+    case object VNil extends VecIC[zero, Nothing]: 
+        def take[i<:Int,j<:Int](using add : Add[i, j, zero]): VecIC[i,Nothing] = add match
+            //Will this work? How do I patttern Match case calss with no parameters
+            case _: NAddZ[_] => VNil
+        def drop[i<:Int,j<:Int](using add : Add[i, j, zero]): VecIC[j,Nothing] = add match
+            case _: NAddZ[_] => VNil
+        
+    case class VCons[n<:Int,+T](hd : T, tl: VecIC[n,T]) extends VecIC[S[n], T]:
+        def take[i<:Int,j<:Int](using add : Add[i,j,S[n]]): VecIC[i,T] = add match 
+            case _: NAddZ[_] => VNil
             case NAddStep(prev) => VCons(hd, tl.take(using prev))
-        def drop[i<:Int,j<:Int](using add : this.s.Add[i,j,Suc[n]]): VecIC[j,T] = add match
-            case NAddZ(_) => this
+        def drop[i<:Int,j<:Int](using add : Add[i,j,S[n]]): VecIC[j,T] = add match
+            case _: NAddZ[_] => this
             case NAddStep(prev) => tl.drop(using prev)
-    */
     
+    
+    type one = 1
+    type i = Int
+    summon[one <:< i]
+    //summon[VecIC[one, Nothing] <:< VecIC[i, Nothing]]
+    
+    
+
     class BnReuseShape extends IndexedComonad[Int, VecIC]:
-        def dup[i <: Int, j<: Int, n <: Int, T](d: VecIC[n, T])(using mul: this.s.Mul[i, j, n]): VecIC[i, VecIC[j, T]] = ???//mul match
-            ///case NMulZLeft(j)        => VNil
-            //case NMulStep(prev,add)  => VCons(take(using add),drop(using add).dup(using prev))
+        def dup[i <: Int, j<: Int, n <: Int, T](d: VecIC[n, T])(using mul: this.s.Mul[i, j, n]): VecIC[i, VecIC[j, T]] = mul match
+            case _: NMulZLeft[_]      => VNil
+            case NMulStep(add, prev)  => VCons(d.take(using add),d.drop(using add).dup(using prev))
         def extract[n <: Int, T](d: VecIC[n, T])(using NotGiven[n =:= this.s.zero]): T = ???
         def map[n <: Int, A, B](d: VecIC[n, A], f: A => B): VecIC[n, B] = ???
