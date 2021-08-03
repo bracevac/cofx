e1dd16ac0a962cced08e577fa4785ab8f4588684
Vaastav Arora <arora74@purdue.edu>
2021-08-02 18:31:41 -0700

    Ctx CCons pending

--- src/main/scala/interp/DecompTest.scala
+++ src/main/scala/interp/DecompTest.scala
@@ -11,35 +11,17 @@ object DecompNew :
 
   trait CoeffScalar[C]:
 
-    type zero <: C
-    type one <: C
+    type use <: C
+    type ign <: C
 
     trait Add[A<:C,B<:C,R<:C]
     trait Mul[A<:C,B<:C,R<:C]
-  
-  /**  
-  trait CoeffScalar[S]:
-
-    type zero <: S
-    type one <: S
- 
-    trait Scalar[S]
-    case object ign extends Scalar[zero]
-    case object use extends Scalar[one]
-    case class Suc(s: S) extends Scalar[S]
-
-    trait Add[A<:S,B<:S,C<:S]
-    trait Mul[A<:S,B<:S,C<:S]
-
-    //case class CSAddZ[r<:S]() extends Add[zero, r, r]
-    //case class CSAddStep[l <: S, r <: S, res <:S](prev : Add[l,r,res]) extends Add[Suc[l], r, Suc[res]]
-  **/
 
 
   trait IndexedComonad[S, D[_ <:S, _], C <: CoeffScalar[S]](using val s: C):
 
     def dup[i <: S, j <: S, n <: S, T](d: D[n, T])(using s.Mul[i, j, n]): D[i, D[j, T]]
-    def extract[n <: S, T](d : D[n,T])(using NotGiven[n =:= s.zero]): T
+    def extract[n <: S, T](d : D[n,T])(using NotGiven[n =:= s.use]): T
     def map[n <: S, A, B](d : D[n,A], f : A => B) : D[n,B]
   
   
@@ -49,8 +31,8 @@ object DecompNew :
   
   trait ScalarMul[S](using val s: CoeffScalar[S]):
     sealed trait ScalarMul[r<:S,I<:Tuple,O<:Tuple]
-    case class SMBase[r<:S](mul : s.Mul[r,s.zero,s.zero]) extends ScalarMul[r,Tup0,Tup0]
-    given smbase[r<:S](using r : s.Mul[r,s.zero,s.zero]) : ScalarMul[r,Tup0,Tup0] = SMBase(r)
+    case class SMBase[r<:S](mul : s.Mul[r,s.use,s.use]) extends ScalarMul[r,Tup0,Tup0]
+    given smbase[r<:S](using r : s.Mul[r,s.use,s.use]) : ScalarMul[r,Tup0,Tup0] = SMBase(r)
     case class SMStep[r<:S,v<:S,rv<:S,I<:Tuple,O<:Tuple](prev: ScalarMul[r,I,O], hdmul: s.Mul[r,v,rv]) extends ScalarMul[r,v *: I, rv *: O]    
     given smstep[r<:S,v<:S,rv<:S,I<:Tuple,O<:Tuple](using hdmul : s.Mul[r,v,rv])(using prev : ScalarMul[r,I,O]) : ScalarMul[r,v *: I, rv *: O] = SMStep(prev,hdmul)
   
@@ -66,14 +48,51 @@ object DecompNew :
   case class Cat2Suc[R1<:Tuple,R2<:Tuple,R12<:Tuple,A1<:Tuple,A2<:Tuple,A12<:Tuple,S,X](prev: Cat2R[R1,R2,A1,A2,R12,A12]) extends Cat2R[S *: R1, R2, X *: A1, A2, S *: R12, X *: A12]
   given cat2s[R1<:Tuple,R2<:Tuple,R12<:Tuple,A1<:Tuple,A2<:Tuple,A12<:Tuple,S,X](using prev : Cat2R[R1,R2,A1,A2,R12,A12]) : Cat2R[S *: R1, R2, X *: A1, A2, S *: R12, X *: A12] = Cat2Suc(prev)
   
-  trait Ctx[S, R <: Tuple, V <: Tuple](using val sm: ScalarMul[S]):
-    def fmap[F <: Tuple, O <: Tuple](f: F)(using ev: MapEv[F, R, O]): Ctx[S, O, V]
-    //Definitely unsafe
-    def extract[n <: Int](using notZero: n > 0): Elem[V, n]
-    def dup[r <: S, I <: Tuple](using smEv: sm.ScalarMul[r,I,R]): Ctx[S, Tup1[r], Tup1[Ctx[S,I,V]]] 
-    def m[R2<:Tuple,V2<:Tuple,R3<:Tuple,V3<:Tuple](that : Ctx[S,R2,V2])(using Cat2R[R,R2,V,V2,R3,V3]): Ctx[S,R3,V3]
-    def n[R1<:Tuple,R2<:Tuple,V1<:Tuple,V2<:Tuple](using Cat2R[R1,R2,V1,V2,R,V]): (Ctx[S,R1,V1], Ctx[S,R2,V2])
+  trait Context[S, D[_<:S,_]](using val sm: ScalarMul[S]):
 
+    trait Ctx[R <: Tuple, V <: Tuple]:
+      def fmap[F <: Tuple, O <: Tuple](f: F)(using ev: MapEv[F, R, O]): Ctx[O, V]
+      //Definitely unsafe
+      def extract[n <: Int](using notZero: n > 0): Elem[V, n]
+      def dup[r <: S, I <: Tuple](using smEv: sm.ScalarMul[r,I,R]): Ctx[Tup1[r], Tup1[Ctx[I,V]]] 
+      def m[R2<:Tuple,V2<:Tuple,R3<:Tuple,V3<:Tuple](that : Ctx[R2,V2])(using cat: Cat2R[R,R2,V,V2,R3,V3]): Ctx[R3,V3]
+      def n[R1<:Tuple,R2<:Tuple,V1<:Tuple,V2<:Tuple](using cat: Cat2R[R1,R2,V1,V2,R,V]): (Ctx[R1,V1], Ctx[R2,V2])
+  
+    case object CNil extends Ctx[Tup0,Tup0]:
+      def fmap[F <: Tuple, O <: Tuple](f: F)(using ev: MapEv[F, Tup0, O]): Ctx[O, Tup0] = ???
+      def extract[n <: Int](using notZero: n > 0): Elem[Tup0, n] = ???
+      def dup[r <: S, I <: Tuple](using smEv: sm.ScalarMul[r,I,Tup0]): Ctx[Tup1[r], Tup1[Ctx[I,Tup0]]] = sm match
+        case sm.SMBase(mulr0) => ???//CCons(VNil.dup(using mulr0).map(_ => CNil), CNil)
+      def m[R2<:Tuple,V2<:Tuple,R3<:Tuple,V3<:Tuple](that : Ctx[R2,V2])(using cat: Cat2R[Tup0,R2,Tup0,V2,R3,V3]): Ctx[R3,V3] = cat match 
+        case Cat2Z() => that
+      def n[R1<:Tuple,R2<:Tuple,V1<:Tuple,V2<:Tuple](using cat: Cat2R[R1,R2,V1,V2,Tup0,Tup0]): (Ctx[R1,V1], Ctx[R2,V2]) = cat match
+        case Cat2Z() => (CNil, CNil)
+      def toList = Nil
+    
+    /*
+    case class CCons[n<:Int,R<:Tuple,A,V <:Tuple](hd : D[n,A], tl : Ctx[R,V])/*(using OK[R,T])*/ extends Ctx[n *: R, A *: V] :
+      //FIXME it seems the compiler is too stupid to figure out congruences like (n *: R) =:= Tup1[m] implies n =:= m, can we do these manually?
+      def fmap[F <: Tuple, O <: Tuple](f: F)(using ev: MapEv[F, R, O]): Ctx[O, V] = ???
+        //mapV[B,m,C](_.map(f))
+      //def mapV[B,m<:Nat,C](f: Vec[m,B] => Vec[m,C])(using (A *: T) =:= Tup1[B], (n *: R) =:= Tup1[m]): Ctx[n *: R, Tup1[C]] =
+      //  CCons(magic(f(magic(hd))), magic(CNil))  
+      def extract[B,m<:Int](using (A *: V) =:= Tup1[B], (n *: R) =:= Tup1[m])(using NotGiven[m =:= Z.type]): Elem[V,n] = ???
+        //magic(hd.extract)
+      def dup[B,i<:Nat,j<:Nat,m<:Nat](using (A *: T) =:= Tup1[B], (n *: R) =:= Tup1[m])(using mul : NMul[i,j,m]): Ctx[Tup1[i], Tup1[Ctx[Tup1[j], Tup1[B]]]] =
+        CCons(hd.dup[i,j](using magic(mul)).map(c => CCons(magic(c),CNil)),CNil)
+      def dup[r<:Nat,S<:Tuple](using sm : ScalarMul[r,S,n *: R]): Ctx[Tup1[r], Tup1[Ctx[S,A *: T]]] = sm match
+        case SMStep(prev, hdmul) => 
+          val CCons(ctl,CNil) = tl.dup(using prev)  //Ctx[<r>, Ctx[S',T]] for some S' w. r ** S' = R
+          val hdd = hd.dup(using hdmul)             //Vec[r, Vec[v,A]
+          CCons((hdd zip ctl) map {case (h,c) => CCons(h,c)},CNil)
+      def m[R2<:Tuple,A2<:Tuple,R3<:Tuple,A3<:Tuple](that : Ctx[R2, A2])(using cat : Cat2R[n *: R,R2,A *: T,A2,R3,A3]): Ctx[R3, A3] = cat match
+        case Cat2Suc(prev) => CCons(hd, tl.m(that)(using prev))
+      def n[R1<:Tuple,R2<:Tuple,A1<:Tuple,A2<:Tuple](using cat : Cat2R[R1,R2,A1,A2,n *: R,A *: T]): (Ctx[R1,A1], Ctx[R2,A2]) = cat match
+        case Cat2Z()        => (CNil, this)
+        case Cat2Suc(prev) => 
+          val (c1, c2) = tl.n(using prev)
+          (CCons(hd,c1), c2)
+      def toList = hd :: tl.toList*/
 
 end DecompNew
 
--- src/main/scala/interp/Examples.scala
+++ src/main/scala/interp/Examples.scala
@@ -9,8 +9,10 @@ import scala.compiletime.{summonFrom, error, erasedValue, summonInline}
 object Scalars: 
 
     implicit object natScalar extends CoeffScalar[Int]:
-        type zero = 0
-        type one = 1
+        type use = 0
+        type ign = 1
+
+        type zero = use
 
         case class NAddZ[r <:Int]() extends Add[zero, r, r]
         case class NAddStep[l <: Int, r <:Int, res <:Int](prev : Add[l,r,res]) extends Add[S[l], r, S[res]]
@@ -32,9 +34,11 @@ object Scalars:
             given mulstep[l <: Int, r <:Int, res <:Int, sum <:Int](using prev : Mul[l,S[r],res], add : Add[S[r],res,sum]) : Mul[S[l],S[r],sum] = NMulStep(add,prev)
         
 
+    implicit object testScalar extends CoeffScalar[Int]
+    
     implicit object boolScalar extends CoeffScalar[Boolean]:
-        type zero = false
-        type one = true
+        type use = false
+        type ign = true
 
         case object BAddFalseFalse extends Add[false, false, false]
         case object BAddFalseTrue extends Add[false, true, false]
@@ -42,67 +46,78 @@ object Scalars:
         case object BAddTrueTrue extends Add[true, true, true]
 
         case object BMulTrueTrue extends Mul[true, true, true]
-        case object BMulTrueFalse extends Add[true, false, true]
-        case object BMulFalseTrue extends Add[false, true, true]
+        case object BMulTrueFalse extends Mul[true, false, true]
+        case object BMulFalseTrue extends Mul[false, true, true]
         case object BMulFalse extends Mul[false, false, false]
 
-
+end Scalars
 
 object Vectors extends App:
 
     import Scalars._
 
-    trait VecIC[n <: Int, +T]:
-        import natScalar._
-        def take[i<:Int,j<:Int](using Add[i, j, n]): VecIC[i,T]
-        def drop[i<:Int,j<:Int](using Add[i, j, n]): VecIC[j,T]
-        //def extract(using NotGiven[n =:= Z.type]): T
+    object Vector:
 
+        import natScalar._
 
-    case object VNil extends VecIC[natScalar.zero, Nothing]: 
-        import natScalar._
-        def take[i<:Int,j<:Int](using add : Add[i, j, zero]): VecIC[i,Nothing] = add match
-            case NAddZ() => VNil
-        def drop[i<:Int,j<:Int](using add : Add[i, j, zero]): VecIC[j,Nothing] = add match
-            case NAddZ() => VNil
-        
-    case class VCons[n<:Int,+T](hd : T, tl: VecIC[n,T]) extends VecIC[S[n], T]:
-        import natScalar._
-        def take[i<:Int,j<:Int](using add : Add[i,j,S[n]]): VecIC[i,T] = add match 
-            case NAddZ() => VNil
-            case NAddStep(prev) => VCons(hd, tl.take(using prev))
-        def drop[i<:Int,j<:Int](using add : Add[i,j,S[n]]): VecIC[j,T] = add match
-            case NAddZ() => this
-            case NAddStep(prev) => tl.drop(using prev)
+        trait IVector[n <: Int, +T]:
+            def take[i<:Int,j<:Int](using Add[i, j, n]): IVector[i,T]
+            def drop[i<:Int,j<:Int](using Add[i, j, n]): IVector[j,T]
+
+        case object VNil extends IVector[use, Nothing]: 
+            def take[i<:Int,j<:Int](using add : Add[i, j, use]): IVector[i,Nothing] = add match
+                case NAddZ() => VNil
+            def drop[i<:Int,j<:Int](using add : Add[i, j, use]): IVector[j,Nothing] = add match
+                case NAddZ() => VNil
+            
+        case class VCons[n<:Int,+T](hd : T, tl: IVector[n,T]) extends IVector[S[n], T]:
+            def take[i<:Int,j<:Int](using add : Add[i,j,S[n]]): IVector[i,T] = add match 
+                case NAddZ() => VNil
+                case NAddStep(prev) => VCons(hd, tl.take(using prev))
+            def drop[i<:Int,j<:Int](using add : Add[i,j,S[n]]): IVector[j,T] = add match
+                case NAddZ() => this
+                case NAddStep(prev) => tl.drop(using prev)
     
-
-    class BnReuseShape extends IndexedComonad[Int, VecIC, natScalar.type](using natScalar):
+    
+    class BnReuseShape extends IndexedComonad[Int, Vector.IVector, natScalar.type](using natScalar):
         import s._
-        def dup[i <: Int, j<: Int, n <: Int, T](d: VecIC[n, T])(using mul: this.s.Mul[i, j, n]): VecIC[i, VecIC[j, T]] = mul match
-            case NMulZLeft()          => VNil
-            case NMulStep(add, prev)  => VCons(d.take(using add), dup(d.drop(using add))(using prev))
-        def extract[n <: Int, T](d: VecIC[n, T])(using NotGiven[n =:= this.s.zero]): T = d match
-            case VCons(hd, tl) => hd
-        def map[n <: Int, A, B](d: VecIC[n, A], f: A => B): VecIC[n, B] = d match
-            case VNil          => VNil
-            case VCons(hd, tl) => VCons(f(hd), map(tl, f))
-    
+        def dup[i <: Int, j<: Int, n <: Int, T](d: Vector.IVector[n, T])(using mul: this.s.Mul[i, j, n]): Vector.IVector[i, Vector.IVector[j, T]] = mul match
+            case NMulZLeft()          => Vector.VNil
+            case NMulStep(add, prev)  => Vector.VCons(d.take(using add), dup(d.drop(using add))(using prev))
+        def extract[n <: Int, T](d: Vector.IVector[n, T])(using NotGiven[n =:= this.s.use]): T = d match
+            case Vector.VCons(hd, tl) => hd
+        def map[n <: Int, A, B](d: Vector.IVector[n, A], f: A => B): Vector.IVector[n, B] = d match
+            case Vector.VNil          => Vector.VNil
+            case Vector.VCons(hd, tl) => Vector.VCons(f(hd), map(tl, f))
+
+    /**
     trait PH[n <: Boolean, T]
     class LivenessShape extends IndexedComonad[Boolean, PH, boolScalar.type](using boolScalar):
         def dup[i <: Boolean, j <: Boolean, n <: Boolean, T](d: PH[n, T])(using s.Mul[i, j, n]): PH[i, PH[j, T]] = ???
-        def extract[n <: Boolean, T](d : PH[n,T])(using NotGiven[n =:= s.zero]): T = ???
+        def extract[n <: Boolean, T](d : PH[n,T])(using NotGiven[n =:= s.use]): T = ???
         def map[n <: Boolean, A, B](d : PH[n,A], f : A => B) : PH[n,B] = ???
+    **/
+end Vectors
 
     
+  
+object Context extends App:
+
+    import Scalars.natScalar
+
+    class Nat extends ScalarMul[Int]
+
     
+
+end Context
     /**
-     * Why not case class NAddZ?
-     * Is this structure good? THe way logic is spread?
-     * Ctx is common across all? Should I have a single Ctx trait and no inhertiance of it?
-     * Does every variable have live or dead state, is there any null?
      * We need to change naming of scalar and indexedcomoonad traits since they're BnReuse specific
-     * Case class the truth table or just four objects each?
-     * You could pass a bad D into IndexedComonad because it doesn't have to match the same COeffect[S] it uses internally
-     * The n>0 in Ctx is still an issue (so are negative integers) 
+     * The n>0 in Ctx is still an issue (so are negative integers) - right ADT for non-zero in a utils file
      * PH placeholder for boolean shape
+     * 
+     * Coeffect[Int] => A, B
+     * VecIC => natScalar
+     * BnReuseShape(B) {
+     *  VecIC[A]
+     * }
      **/
